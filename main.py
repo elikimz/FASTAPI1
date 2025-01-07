e714b2fd78a36467a5b46ed17587cd7ef8828fc7
elikimz <elijahkimani1293@gmail.com>
2025-01-07 16:15:35 +0300

    first commit

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ app=FastAPI()
 @app.get("/")
 def root():
     return {"message": "Welcome to my FASTAPI"}
+
+@app.get("/posts")
+def get_posts():
+    return{"data":"This is your posts"}
+@app.post("/createposts")
+def create_posts():
+    return{"data":"posts created successifully"}
+            

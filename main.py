666d077dd9e23779beec8f8b32f96ccaf3abdebf
elikimz <elijahkimani1293@gmail.com>
2025-01-08 16:08:51 +0300

    commit

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import time
+from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from psycopg2.extras import  RealDictCursor
+import psycopg2
+
 app=FastAPI()
 
 class Posts(BaseModel):
     title:str
     content:str
     published:bool=True
+    rating:Optional[int]=None
+
+
+try:
+     conn = psycopg2.connect(host='localhost', database='postgres',user="postgres", password='40284433',
+     cursor_factory=RealDictCursor)
+     cursor = conn.cursor()
+     print("Database connected successfully")
+except Exception as error:
+    print("Connection to database failed") 
+    print("Error:",error) 
+    time.sleep(2)   
+
 
 
 @app.get("/")
@@ -18,6 +36,6 @@ def get_posts():
 
 @app.post("/createposts")
 def create_posts(new_post:Posts):
-    print(new_post)
-    return{"data": "new_post"}
+    print(new_post.dict())
+    return{"data": new_post}
             

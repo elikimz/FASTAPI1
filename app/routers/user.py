d04753e4dd8d9fc202483597b8cde23365c9d2d4
elikimz <elijahkimani1293@gmail.com>
2025-01-25 15:27:03 +0300

    commit

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 
 router=APIRouter(
-     prefix="/users",
+
      tags=['USERS']
 )
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 def create_user(new_user: schemas.UserCreate, db: Session = Depends(get_db)):
     # Check if the user already exists
     existing_user = db.query(models.User).filter(models.User.email == new_user.email).first()
@@ -33,7 +33,7 @@ def create_user(new_user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 
-@router.get('/{id}',response_model=schemas.UserResponse)
+@router.get('/users/{id}',response_model=schemas.UserResponse)
 def get_user(id:int,db:Session = Depends(get_db)):
     user=db.query(models.User).filter(models.User.id==id).first()
     print(user)

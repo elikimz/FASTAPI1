f6c8fe1012d83cbe584d25499230f9b1b0bfc856
elikimz <elijahkimani1293@gmail.com>
2025-01-25 16:25:16 +0300

    commit

--- app/config.py
+++ app/config.py
@@ -1,24 +1,49 @@
 
-from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
+# from pydantic_settings import BaseSettings
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-class Setting(BaseSettings):
-    database_hostname:str
-    database_port:str
-    database_password:str
-    database_name:str
-    database_username:str
-    secret_key:str
-    algorithm:str
-    access_token_expire_minutes:int
+# class Setting(BaseSettings):
+#     database_hostname:str
+#     database_port:str
+#     database_password:str
+#     database_name:str
+#     database_username:str
+#     secret_key:str
+#     algorithm:str
+#     access_token_expire_minutes:int
      
 
+#     class Config:
+#        env_file=".env"
+
+# setting =Setting()    
+
+# print(setting.database_hostname)
+# print(setting.database_port)
+
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Make sure the .env file is loaded
+
+class Setting(BaseSettings):
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
     class Config:
-       env_file=".env"
+        env_file = ".env"
 
-setting =Setting()    
+setting = Setting()
 
-print(setting.database_hostname)
-print(setting.database_port)
+# Print the values to check if they are correctly loaded from the .env file
+print("Database Hostname:", setting.database_hostname)  # should print dpg-cuad03dumphs73ckdr1g-a
+print("Database Port:", setting.database_port)  # should print 5432

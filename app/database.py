a64f96c39ab9d497021ec131d457cf3e5c508cf5
elikimz <elijahkimani1293@gmail.com>
2025-01-25 15:56:55 +0300

    commit

--- app/database.py
+++ app/database.py
@@ -1,52 +1,24 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from .config import Setting
-
-# setting = Setting()
-# SQLALCHEMY_DATABASE_URL= f'postgresql://{setting.database_username}:' \
-# f'{setting.database_password}@' \
-# f'{setting.database_hostname}:'\
-# f'{setting.database_port}/'\
-# f'{setting.database_name}'
-
-# engine=create_engine(SQLALCHEMY_DATABASE_URL)
-
-# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
-# Base=declarative_base()
-
-# def get_db():
-#      db=SessionLocal()
-#      try:
-#           yield db
-#      finally:
-#           db.close()
-
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+from .config import Setting
 
-# Load environment variables from the .env file
-load_dotenv()
+setting = Setting()
+SQLALCHEMY_DATABASE_URL= f'postgresql://{setting.database_username}:' \
+f'{setting.database_password}@' \
+f'{setting.database_hostname}:'\
+f'{setting.database_port}/'\
+f'{setting.database_name}'
 
-# Get the DATABASE_URL from the environment
-DATABASE_URL = os.getenv("DATABASE_URL")
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Create the SQLAlchemy engine using the DATABASE_URL
-engine = create_engine(DATABASE_URL)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+Base=declarative_base()
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+     db=SessionLocal()
+     try:
+          yield db
+     finally:
+          db.close()

f8125fab73ad6ebf667c5897af21d6f6379169f4
elikimz <elijahkimani1293@gmail.com>
2025-01-26 13:23:51 +0300

    commit

--- app/config.py
+++ app/config.py
@@ -1,44 +1,28 @@
 
-# from pydantic_settings import BaseSettings
-# from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
-# load_dotenv()
-# class Setting(BaseSettings): 
-#  database_port: int 
-#  database_username: str 
-#  database_hostname: str 
-#  database_password: str 
-#  database_name: str 
-#  secret_key: str 
-#  algorithm: str 
-#  access_token_expire_minutes: int 
+load_dotenv()
+class Setting(BaseSettings): 
+ database_port: int 
+ database_username: str 
+ database_hostname: str 
+ database_password: str 
+ database_name: str 
+ secret_key: str 
+ algorithm: str 
+ access_token_expire_minutes: int 
      
 
     
-# class Config:
-#        env_file=".env"
+class Config:
+       env_file=".env"
 
-# setting =Setting()    
+setting =Setting()    
 
-# print(setting.database_hostname)
-# print(setting.database_port)
-# print(setting.database_password)
-# print(setting.database_username)
+print(setting.database_hostname)
+print(setting.database_port)
+print(setting.database_password)
+print(setting.database_username)
 
 
-from pydantic_settings import BaseSettings  # Changed from pydantic to pydantic_settings
-from dotenv import load_dotenv
-
-# Load environment variables from the .env file
-load_dotenv()
-
-class Setting(BaseSettings):
-    database_url: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-
-    class Config:
-        env_file = ".env"  # this tells pydantic to look for the .env file
-
-setting = Setting()  # Instantiate the settings
--- app/database.py
+++ app/database.py
@@ -3,47 +3,23 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import Setting
 
-# setting = Setting()
-# SQLALCHEMY_DATABASE_URL= f'postgresql://{setting.database_username}:' \
-# f'{setting.database_password}@' \
-# f'{setting.database_hostname}:'\
-# f'{setting.database_port}/'\
-# f'{setting.database_name}'
+setting = Setting()
+SQLALCHEMY_DATABASE_URL= f'postgresql://{setting.database_username}:' \
+f'{setting.database_password}@' \
+f'{setting.database_hostname}:'\
+f'{setting.database_port}/'\
+f'{setting.database_name}'
 
-# engine=create_engine(SQLALCHEMY_DATABASE_URL)
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
-# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
-# Base=declarative_base()
+Base=declarative_base()
 
-# def get_db():
-#      db=SessionLocal()
-#      try:
-#           yield db
-#      finally:
-#           db.close()
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from .config import setting  # Import the settings instance
-
-# Use the DATABASE_URL from environment variables for the Neon database connection
-SQLALCHEMY_DATABASE_URL = setting.database_url
-
-# Create the database engine using the constructed URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# Create a session maker bound to the engine
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for ORM models
-Base = declarative_base()
-
-# Dependency for getting the database session
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+     db=SessionLocal()
+     try:
+          yield db
+     finally:
+          db.close()
+

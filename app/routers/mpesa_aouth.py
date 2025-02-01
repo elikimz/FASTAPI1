0409f54388a604d67e31b498e8c6ebbe63cc84cf
elikimz <elijahkimani1293@gmail.com>
2025-02-01 19:06:26 +0300

    commit

--- app/routers/mpesa_aouth.py
+++ app/routers/mpesa_aouth.py
@@ -1,13 +1,13 @@
 import requests
 import base64
 import datetime
-from ..config import MPESA_CONSUMER_KEY,MPESA_CONSUMER_SECRET,MPESA_PASSKEY,MPESA_SHORTCODE,CALLBACK_URL
+from ..config import setting
 
 # Function to get M-Pesa access token
 def get_mpesa_token():
     url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-    response = requests.get(url, auth=(MPESA_CONSUMER_KEY, MPESA_CONSUMER_SECRET))
-    
+    response = requests.get(url, auth=(setting.MPESA_CONSUMER_KEY, setting.MPESA_CONSUMER_SECRET))
+
     if response.status_code == 200:
         return response.json().get("access_token")
     else:
@@ -16,20 +16,20 @@ def get_mpesa_token():
 # Function to initiate an STK Push request
 def stk_push_request(phone_number, amount):
     access_token = get_mpesa_token()
-    
+
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    password = base64.b64encode(f"{MPESA_SHORTCODE}{MPESA_PASSKEY}{timestamp}".encode()).decode()
+    password = base64.b64encode(f"{setting.MPESA_SHORTCODE}{setting.MPESA_PASSKEY}{timestamp}".encode()).decode()
 
     payload = {
-        "BusinessShortCode": MPESA_SHORTCODE,
+        "BusinessShortCode": setting.MPESA_SHORTCODE,
         "Password": password,
         "Timestamp": timestamp,
         "TransactionType": "CustomerPayBillOnline",
         "Amount": amount,
         "PartyA": phone_number,
-        "PartyB": MPESA_SHORTCODE,
+        "PartyB": setting.MPESA_SHORTCODE,
         "PhoneNumber": phone_number,
-        "CallBackURL": CALLBACK_URL ,
+        "CallBackURL": setting.CALLBACK_URL,
         "AccountReference": "Test",
         "TransactionDesc": "Payment"
     }
